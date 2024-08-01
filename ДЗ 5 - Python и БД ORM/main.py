53c562943e7c0be2d88d3d6595d214b591ff6b91
Sergey Tormozov <web@stormozov.ru>
2024-08-01 22:30:38 +0300

    ДЗ 5 - Добавляю решение v1.05
    Добавлен новый модуль, который позволяет получить абсолютный путь к файлу.

--- ДЗ 5 - Python и БД ORM/main.py
+++ ДЗ 5 - Python и БД ORM/main.py
@@ -2,10 +2,16 @@ from modules.read_config.read_config import read_config
 from modules.db_session.create_db_session import create_database_session
 from modules.db_management.db_operations import *
 from modules.json_to_db.json2db import import_json_data_to_db
+from modules.publisher_reports.publisher_sales import get_publisher_sales_report
+from modules.fs_tools.path_utils import get_absolute_path
 
 if __name__ == '__main__':
+	# Create paths
+	json_data = get_absolute_path(['source', 'tests_data.json'])
+	settings_data = get_absolute_path(['settings.ini'])
+
 	# Read config file
-	config_dict = read_config('settings.ini')
+	config_dict = read_config(settings_data)
 
 	# Create engine for the database
 	engine = create_database_session(config_dict)[1]
@@ -16,11 +22,12 @@ if __name__ == '__main__':
 	# Create session for the database
 	session = create_database_session(config_dict)[0]
 
-	# Upload data from json file to database
-	import_json_data_to_db(session, 'source/tests_data.json')
+	# (Task #3) Upload data from json file to database
+	import_json_data_to_db(session, json_data)
 
-	# input_publisher = input("Enter the publisher's name or ID: ")
-	# get_publisher_sales_report(config_dict, input_publisher)
+	# (Task #2) Get sales report
+	input_publisher = input("Enter the publisher's name or ID: ")
+	get_publisher_sales_report(config_dict, input_publisher)
 
 	# Close the session
 	session.close()
